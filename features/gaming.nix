d200ee234d07b7db8357175ee4857faf99ea6e7d
gabriel aguiar rocha <gabriel.rag@proton.me>
2026-06-25 10:50:12 -0400

    refactor(features): completely remove legacy gaming wrapper

--- features/gaming.nix
+++
@@ -1,7 +0,0 @@
-# TODO: Módulo legado. Será removido após unificação completa das features.
-# A implementação real foi movida para modules/nixos/features/gaming.nix.
-{
-  imports = [
-    ../modules/nixos/features/gaming.nix
-  ];
-}
