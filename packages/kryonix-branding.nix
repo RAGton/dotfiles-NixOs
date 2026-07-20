fa1c798adfa6a2478d22aaf1e56459d55edfcf38
Gabriel Aguiar Rocha (RAGton) <aguiarrocha37@outlook.com>
2026-07-20 19:49:03 -0400

    fix(branding): add dummy assets to prevent build failures on sddm and kde

--- packages/kryonix-branding.nix
+++ packages/kryonix-branding.nix
@@ -21,8 +21,14 @@ stdenvNoCC.mkDerivation {
       "$out/share/backgrounds/kryonix" \
       "$out/share/pixmaps/kryonix"
 
-    # Removed cp commands since desktop/branding/kryonix does not exist in this repo.
-
+    # Create dummy SVGs since the original assets were deleted by the user
+    install -dm755 "$out/share/icons/hicolor/scalable/apps"
+    echo '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100"><rect width="100" height="100" fill="#000000"/></svg>' > "$out/share/pixmaps/kryonix/logo.svg"
+    echo '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100"><rect width="100" height="100" fill="#000000"/></svg>' > "$out/share/icons/hicolor/scalable/apps/kryonix-logo.svg"
+    echo '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1920 1080"><rect width="1920" height="1080" fill="#121212"/></svg>' > "$out/share/backgrounds/kryonix/kryonix-clean-dark.svg"
+    echo '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1920 1080"><rect width="1920" height="1080" fill="#ffffff"/></svg>' > "$out/share/backgrounds/kryonix/kryonix-clean-light.svg"
+    echo '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1920 1080"><rect width="1920" height="1080" fill="#1e1e2e"/></svg>' > "$out/share/backgrounds/kryonix/kryonix-blue-glass-dark.svg"
+    echo '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1920 1080"><rect width="1920" height="1080" fill="#eff1f5"/></svg>' > "$out/share/backgrounds/kryonix/kryonix-blue-glass-light.svg"
     runHook postInstall
   '';
 
