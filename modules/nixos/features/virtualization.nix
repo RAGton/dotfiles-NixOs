79f964460e561befa71a53c6aa4f8c98976df85a
Gabriel Aguiar Rocha <aguiarrocha37@outlook.com>
2026-08-28 08:01:02 -0400

    fix(virtualization): fix polkit option name from extraRules to extraConfig

--- modules/nixos/features/virtualization.nix
+++ modules/nixos/features/virtualization.nix
@@ -178,7 +178,7 @@ in
     };
 
     # Polkit: permite gestão do libvirt (virsh / virt-manager) sem senha para usuários no grupo libvirtd
-    security.polkit.extraRules = lib.mkIf (cfg.kvm.enable && cfg.libvirt.enable) ''
+    security.polkit.extraConfig = lib.mkIf (cfg.kvm.enable && cfg.libvirt.enable) ''
       polkit.addRule(function(action, subject) {
         if (action.id.indexOf("org.libvirt.unix.") === 0 && subject.isInGroup("libvirtd")) {
           return polkit.Result.YES;
